a603fe9bd35f148a04b23119574a16ca7bda5408
Kind <alvaromoreno21197@gmail.com>
2022-10-31 14:21:32 +0100

    FEAT INIT

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import Characters from './pages/characters';
+import Home from './pages/home';
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <h1>APP WORKING</h1>
+    <Home/>
+    <Characters/>
+    </div>
+  );
+}
+
+export default App;
